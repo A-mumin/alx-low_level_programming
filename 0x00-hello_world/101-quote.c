83ee81e627b62867dd5066fd7ee143102649db12
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-12 05:09:07 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpoar, 2015-10-19,'\n");
+		return (0);
+}
