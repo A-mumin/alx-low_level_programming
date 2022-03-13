330bf49f9b68dd295b964af92f2bf5725a70ea3c
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-13 15:46:35 -0700

    print_number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always zero (succefful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <=57; i++)
+	{
+		putchar (i);
+	}
+	putchar('\n');
+
+	return (0);
+}
