a91eca015f60ccf2751def41b165c0d0f363a51e
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-13 15:25:24 -0700

    expec_q_e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero (sucessful)
+ */
+int main(void)
+{
+	char lowercase
+		for(lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		{
+			if (lowercase == 'q')
+				continue;
+			else if (lowercase == 'e')
+				continue;
+			putchar(lowercase);
+		}
+	putchar('\n');
+
+	return (0);
+}
