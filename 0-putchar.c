e42a910fce9e32b2dcc7a4a0469324f2910021c6
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-15 15:03:12 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry  point
+ *
+ * Return: Always zero (success)
+ */
+int main(long long _putchar)
+{
+	putchar('_putchar');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My Alx task for 0x02-functions nested loops
