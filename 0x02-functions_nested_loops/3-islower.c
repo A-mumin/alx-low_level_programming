1f9d26a9d52b68a223b358712f664621d88d5a0d
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-15 20:13:48 -0700

    is_lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * Return: 1 if c is lowercase otherwise zero
+ */
+int _islower(int c)
+{
+	char lc;
+
+	for (lc = 033; lc <= 127; lc++)
+	{
+		if (lc = 097; lc <= 122;)
+		{
+			return (1);
+		}
+		else
+			return(0);
+	}
+}
