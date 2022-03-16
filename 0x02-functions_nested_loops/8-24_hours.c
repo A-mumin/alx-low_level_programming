470849e66651f0e88deeb7d235ac17928ccddfd2
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-16 16:34:00 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /**
  * jack_bauer - Prints every minute of he day
- * 
+ *
  * Return: Always 0
  */
 void jack_bauer(void)
 {
 	int n;
-	
+
 	_putchar (0);
 
 	for (n = '0'; n <= '9'; n++)
