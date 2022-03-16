62380fe144c467ec91319457cbe9f61440060bae
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-15 18:42:27 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always zero 
+ */
+int main(void)
+{
+	char lc;
+
+		for (lc == a; lc <= z; lc++)
+		{
+			putchar(lc);
+		}
+		putchar('\n');
+
+	return (0);
+}
