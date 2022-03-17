5d3833fe10e2e24d52e97bae8cbb062eb30dd754
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-16 19:25:30 -0700

    happy

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (i =n; i > 97; i--)
+		for (i = n; i > 97; i--)
 		{
-			printf ("%d", i);
+			printf("%d", i);
 			if (i != 98)
 			{
-				printf (", ");
+				printf(", ");
 			}
 		}
 	}
@@ -24,12 +24,12 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 99; i++)
 		{
-			printf ("%d", i);
+			printf("%d", i);
 			if (i != 98)
 			{
 				printf(", ");
 			}
 		}
 	}
-	printf ("\n");
+	printf("\n");
 }
