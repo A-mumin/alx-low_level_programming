fa9886c71d1b11d32b0ad60709c1633cd941743b
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-20 03:54:11 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/**
+
+/*
  * while (i < 10)
  *{
- * causes the infinite
+ * Infinite loop - No increase of variable
  * putchar(i);
  }
  */
