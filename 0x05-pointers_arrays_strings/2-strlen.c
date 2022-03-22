a06facfab15b99809206dd2cd6cb6680ac9026c4
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-21 19:11:50 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: variable character
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int l;
+
+	while (s(l) != '\0')
+	{
+		l++;
+	}
+	return (l);
+}
