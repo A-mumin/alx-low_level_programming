a356284d424a0855fc249d37d4c1ceffd845dc67
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-21 19:33:10 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string 
+ * @str: character variable
+ * Return: Always zero
+ */
+
+void _puts(char *str);
+{
+	_puts(str);
+}
