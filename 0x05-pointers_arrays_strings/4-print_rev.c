e51d1f8fecf80fd9435fb7c0c5911b94257ee57d
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-23 14:44:47 -0700

    oo!

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev function that prints a string
+ * in reverse
+ * @s: string
+ * Return: zero successful
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i = i -1; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
