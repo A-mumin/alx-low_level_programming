c1daa615331b508c918532641d912d616e0f5bb5
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-24 16:36:25 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	char *str_1 = s1;
 	char *str_2 = s2;
 
-	while (*str_1 != '\0' && *str_2 != '\0' && str_1 == str_2)
+	while (*str_1 != '\0' && *str_2 != '\0' && *str_1 == *str_2)
 	{
 		str_1++;
 		str_2++;
