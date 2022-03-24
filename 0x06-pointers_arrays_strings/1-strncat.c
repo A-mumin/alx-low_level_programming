b5f881e88a1ab60503db2066d00971dc5648a6d4
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-24 16:15:36 -0700

    append and

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - append a string to another string but dont null-terminte if n bytes
+ * @dest: destination string 
+ * @src: source string
+ * @n: number of btyes
+ * Return: destination string
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
