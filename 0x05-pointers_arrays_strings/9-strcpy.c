fa3e612b0300bbcccef81f5dfaab586c1250b086
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-24 04:43:33 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that cpies the sring pointed by src, inlcuding zero,
+ * to the buffer pointed by dest
+ * @dest: A buffer pointer
+ * @src: pointer to a string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while ( src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
