d75ddc7552e1f6fbcdc4c3cb64d563a796d6fa8f
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-26 15:08:20 -0700

    nine

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * main - function that prints the numbers from 1 to 100
+ *
+ * @Return: always zero
+ */
+
+int main(void)
+{
+	int p = 100;
+	int i;
+
+	i = 1;
+	while (i <= p)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuZZ ");
+		}
+		else if (1 % 5 == 0)
+		{
+			if (i < p)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
