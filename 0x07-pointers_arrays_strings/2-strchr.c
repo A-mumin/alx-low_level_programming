3fa1c4089586b602bd57a18851b3226370747695
A-mumin <aliyuabdulmumin119@gmail.com>
2022-03-28 15:50:41 -0700

    two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string to be checked
+ * @c: character in the string
+ * @Return: furst c in s or NULL if c not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+	{
+		return (s + i);
+	}
+
+	return (0);
+}
