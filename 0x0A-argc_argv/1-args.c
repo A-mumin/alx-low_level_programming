a00b6a31a69bd469b88cef4f13d1c5cd46ce5859
A-mumin <aliyuabdulmumin119@gmail.com>
2022-04-01 16:53:21 -0700

    one

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument array
+ * @Return: returns zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
