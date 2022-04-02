eea59ff80a103e0f2abc61ee82fd60b373f7a987
A-mumin <aliyuabdulmumin119@gmail.com>
2022-04-02 07:06:04 -0700

    four

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
 	if (argc == 3)
 	{
 	Ans = a * b;
-	printf ("%d", Ans);
+	printf ("%d\n", Ans);
 	}
 	else
 	{
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument array
+ * @Return: always zero
+ */
+
+int main(int argc, char* argv[])
+{
+	int i, sum;
+
+	i = 0;
+	sum = 0;
+
+	if (*argv[i] >= 'A' && *argv[i] <= 'Z')
+	{
+		printf ("Error\n");
+	}
+	else if (*argv[i] >= 'a' && *argv[i] <= 'z')
+	{
+		printf ("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		printf ("%d\n", sum);
+	}		
+	return (0);
+}
