b17961bc08411fc512830c7b9df245a48f154071
A-mumin <aliyuabdulmumin119@gmail.com>
2022-04-01 17:21:24 -0700

    zero

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,12 @@
  * @Return: always zero
  */
 
-int main(int agrc, char *argv[])
+int main(int agrc, char* argv[])
 {
+	if (agrc == 0)
+	{
+		printf ("\n");
+	}
 	printf ("%s\n", argv[0]);
 	return (0);
 }
